25c187f94f65fb01d94df9a9cea39acab4ccf0d5
misaka.eth <misaka.eth@gmail.com>
2022-05-11 17:58:02 +0300

    Fix polygon parsing and add connect float parsing

---
+++ potoforio/helpers/parsers.py
@@ -0,0 +1,38 @@
+def parse_float(float_str: str) -> (int, int):
+    """
+    Parse float to int, and it's power
+    Example: 44.887088640175348549 -> (44887088640175348549, 18)
+    """
+    # Correct process 0
+    if float_str == '0':
+        return 0, 1
+
+    # Split
+    before, after = float_str.split(".")
+
+    # Check is it's can be parsed to int
+    int(before)
+    int(after)
+
+    # Make
+    full = int(f'{before}{after}')
+
+    return full, len(after)
+
+
+def normalize_power(base: int, power: int, normal_power: int) -> (int, int):
+    """
+    Return base in normal_power.
+    Can LOSE PRECISION if power>normal_power
+    """
+    power_diff = normal_power - power
+
+    if power_diff > 0:
+        base = base * pow(10, power_diff)
+        return base, normal_power
+
+    if power_diff < 0:
+        base = int(base / pow(10, -power_diff))
+        return base, normal_power
+
+    return base, normal_power
--- potoforio/providers/polygonscan/polygonscan.py
+++ potoforio/providers/polygonscan/polygonscan.py
@@ -2,6 +2,7 @@ from potoforio.providers import BalanceProvider
 from bs4 import BeautifulSoup
 
 from potoforio.core.models import Wallet, Blockchain, AssetOnBlockchain
+from potoforio.helpers.parsers import parse_float, normalize_power
 
 
 class PolygonScan(BalanceProvider):
@@ -12,30 +13,56 @@ class PolygonScan(BalanceProvider):
         self._unknown_assets = []
 
     async def scan_wallet(self, wallet: Wallet):
-        url = f"https://polygonscan.com/address/{wallet.address}"
+        blockchain_polygon = Blockchain.objects.filter(name=self.BLOCKCHAIN_NAME).last()
+
+        url = f'https://polygonscan.com/address/{wallet.address}'
         response = await self._request('GET', url)
         response = await response.text()
 
         soup = BeautifulSoup(response, 'html.parser')
 
-        erc20_assets = soup.find_all(class_="list-custom-ERC-20")
+        matic_on_polygon = AssetOnBlockchain.objects.filter(address='0x0000000000000000000000000000000000001010').last()
+
+        balance = soup.find_all(class_='col-md-8')
+        balance = balance[0].text.replace(' MATIC ', '').replace(',', '')
+
+        # Process parsed string to correct balance
+        base, power = parse_float(balance)
+        balance, _ = normalize_power(base, power, matic_on_polygon.asset.decimals)
+
+        # Save balance only if it's non-zero
+        if balance:
+            await self._update_balance(
+                wallet=wallet,
+                blockchain=blockchain_polygon,
+                asset=matic_on_polygon.asset,
+                balance=balance
+            )
+
+        erc20_assets = soup.find_all(class_='list-custom-ERC-20')
         for erc20_asset in erc20_assets:
             href = erc20_asset.find('a').get('href')
-            amount, ticker = erc20_asset.find(class_='list-amount').text.split()
-            amount = amount.replace(',', "")
+            balance, ticker = erc20_asset.find(class_='list-amount').text.replace(',', '').split()
             asset_address = href.split("?")[0].split("/")[2]
-            self._logger.debug(f"{wallet.address}: {amount}{ticker}")
 
-            blockchain_polygon = Blockchain.objects.filter(name=self.BLOCKCHAIN_NAME).last()
+            # Skip MATIC token, due to incorrect representation in list
+            if asset_address == '0x0000000000000000000000000000000000001010':
+                continue
+
+            self._logger.debug(f'Found asset {wallet.address}: {balance}{ticker}')
+
             asset_on_polygon = AssetOnBlockchain.objects.filter(address=asset_address).last()
 
             if not asset_on_polygon:
+                # Show record in log only one time per session
                 if asset_address not in self._unknown_assets:
                     self._unknown_assets.append(asset_address)
-                    self._logger.warning(f"Unknown asset: {amount} {ticker} with address {asset_address}")
+                    self._logger.warning(f'Unknown asset: {balance} {ticker} with address {asset_address}')
                 continue
 
-            balance = str(int(float(amount) * pow(10, asset_on_polygon.asset.decimals)))
+            # Process parsed string to correct balance
+            base, power = parse_float(balance)
+            balance, _ = normalize_power(base, power, asset_on_polygon.asset.decimals)
 
             await self._update_balance(
                 wallet=wallet,
